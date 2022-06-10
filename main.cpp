1699b59bf1ec95b74b4ee39c99e0f2bee029c5b2
vbscript055246 <30942066+vbscript055246@users.noreply.github.com>
2022-06-10 16:41:14 +0800

    fix for linux

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <conio.h>
-#include <stdlib.h>
-#include <windows.h>
+#include <thread>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
 #include <unistd.h>
-#include <string.h>
-#include <time.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
 using namespace std;
 
 class node;
@@ -102,6 +109,26 @@ class food{
 		}
 };
 
+void read_keyboard();
+
+int c_getche(void)
+{
+  struct termios old, _new;
+  int ch;
+
+  tcgetattr(0, &old);
+
+  _new = old;
+  _new.c_lflag &= ~ICANON;
+  //new.c_lflag &= ~ECHO;
+  tcsetattr(0, TCSANOW, &_new);
+
+  ch = getchar();
+
+  tcsetattr(0, TCSANOW, &old);
+  return ch;
+}
+
 void clean_map(int** map){
 	for(int i=0;i<25;i++)
 		for(int j=0;j<25;j++)
@@ -129,51 +156,25 @@ int main(){
 	temp.y = 12;
 	SK.push_back(temp); 
 	map[12][12] = 1;
-
-	char C;
-	char *ch;
 	
 	food FD(map);
 
-	while(1){
-		
-        	if (GetAsyncKeyState(87) == -32767 && d!=4)
-        		d = 2;
-        	if (GetAsyncKeyState(65) == -32767 && d!=3)
-        		d = 1;
-        	if (GetAsyncKeyState(83) == -32767 && d!=2)
-        		d = 4;
-        	if (GetAsyncKeyState(68) == -32767 && d!=1)
-        		d = 3;
-        	/*	
-        	if (GetAsyncKeyState(48) == -32767){
-        		temp.FNB = SK.size()-1;
-        		SK.push_back(temp);
-			update_map(map,FD);
-        	}
-        	*/
-        	if(FD.check()){
-        		temp.FNB = SK.size()-1;
-        		SK.push_back(temp);
-			update_map(map,FD);
-			FD.to_food(map);
-        	}
+    thread keyboard_thread(read_keyboard);
+
+	while(1){  
+
+        if(FD.check()){
+            temp.FNB = SK.size()-1;
+            SK.push_back(temp);
+            update_map(map,FD);
+            FD.to_food(map);
+        }
         
 		update_map(map,FD);
 		
 		string str;
-		ch = "╔";
-		str.push_back(ch[0]);
-		str.push_back(ch[1]);
-		for(int i=0;i<12;i++){
-			ch = "═";
-			str.push_back(ch[0]);
-			str.push_back(ch[1]);
-		}
-		ch = "╗";
-		str.push_back(ch[0]);
-		str.push_back(ch[1]);
-		
+        for(int i=0;i<26;i++)
+			str.push_back('X');
 		str.push_back('\n');
 		
 		for(int i=0;i<25;i++){
@@ -200,34 +201,28 @@ int main(){
 			str.push_back('\n');
 		}
 		
-		ch = "╚";
-		str.push_back(ch[0]);
-		str.push_back(ch[1]);
-		
-		for(int i=0;i<12;i++){
-			ch = "═";
-			str.push_back(ch[0]);
-			str.push_back(ch[1]);
-		}
-		
-		ch = "╝";
-		str.push_back(ch[0]);
-		str.push_back(ch[1]);
-		
+		for(int i=0;i<26;i++)
+			str.push_back('X');
 		str.push_back('\n');
 		
 		cout << str << SK.size() << endl;
 		
-		/*
-		for(int i = 0;i<25;i++){
-			for(int j = 0;j<25;j++){
-				cout << map[i][j];
-			}
-			cout << endl;
-		}
-		*/
-		
-		usleep(50000);
-		system("cls");	
+		usleep(250000);
+		system("clear");	
 	} 
-} 
+}
+
+
+void read_keyboard(){
+    while(1){
+        char __c = c_getche();
+        if ( __c == 'w' && d!=4)
+            d = 2;
+        if (__c == 'a' && d!=3)
+            d = 1;
+        if (__c == 's' && d!=2)
+            d = 4;
+        if (__c == 'd' && d!=1)
+            d = 3;
+    }
+}
